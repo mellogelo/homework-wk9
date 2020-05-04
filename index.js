d19aac56bc969a4769f70bac28f2092b459ccdf5
Ralph Magbanua <rmagbanua09@gmail.com>
2020-05-03 23:08:41 -0700

    added prompt and writefile

--- index.js
+++ index.js
@@ -4,18 +4,39 @@ var inquirer = require("inquirer");
 inquirer
     .prompt([
     {
-        type:"input",
+        type: "input",
         message: "This is a test",
-        name: "question1"
+        name: "projectTitle"
+    },
+    {
+        type: "input",
+        message: "This is a test",
+        name: "description"
+    },
+    {
+        type: "checkbox",
+        message: "Choose a License",
+        name: "license",
+        choices: [
+            "MIT",
+            "Apache",
+            "GNU"
+        ]
     }
 ])
 .then(function(data) {
-    fs.appendFile("READ.md", "utf8", function(err){    
+
+console.log(data)
+let readmeInfo = 
+`# ${data.projectTitle}
+## ${data.description}`
+
+       
+        fs.writeFile("README.md", readmeInfo + '\n', function(err){    
 
         if(err) {
             return console.log(err);
         }
-    
-        console.log("Success!")
+        console.log("Success! You've created a README")
     })
 })
