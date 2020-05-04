50b7cb4abb0c86ea7ece6eeaff0cf607d4c3fe4a
Ralph Magbanua <rmagbanua09@gmail.com>
2020-05-03 21:15:30 -0700

    added index.js, inquire, fs and npm

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var fs = require("fs")
+var inquirer = require("inquirer");
 
+inquirer
+    .prompt([
+    {
+        type:"input",
+        message: "This is a test",
+        name: "question1"
+    }
+])
+.then(function(data) {
+    fs.appendFile("READ.md", "utf8", function(err){    
+
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("Success!")
+    })
+})
